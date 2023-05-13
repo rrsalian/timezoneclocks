d481374a292aff79a56e1856fd6395dad587ec76
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-05-13 13:39:57 -0400

    Clock display

--- src/components/ClockDisplay/ClockDisplay.tsx
+++ src/components/ClockDisplay/ClockDisplay.tsx
@@ -1,12 +1,10 @@
 import { useState,useEffect } from "react";
 import { AnalogClock } from "../AnalogClock/AnalogClock"
 
-//import "./ClockDisplay.css"
-
 function getCurrentTime() :string {
 
   return (new Intl.DateTimeFormat([], {
-    timeZone: 'Australia/Sydney',   
+    timeZone: 'Australia/Sydney',
     hour: 'numeric',
     minute: 'numeric',
     second: 'numeric'
@@ -14,21 +12,10 @@ function getCurrentTime() :string {
   
 }
 
-// props: { clocks: Clock[]}
 export function ClockDisplay() {
 
     const [ time, setTime ] = useState(getCurrentTime().split(":"));
     
-    // const secondsStyle = {
-    //   transform: `rotate(${+time[2] * 6}deg)`
-    // };
-    // const minutesStyle = {
-    //   transform: `rotate(${+time[1] * 6}deg)`
-    // };
-    // const hoursStyle = {
-    //   transform: `rotate(${+time[0] * 30}deg)`
-    // };
-
     useEffect(()=> {
 
       let timer = setInterval(() => setTime(getCurrentTime().split(":")), 1000);
@@ -41,20 +28,11 @@ export function ClockDisplay() {
 
     return (
         <div>
-          <AnalogClock time={time}></AnalogClock>
-
-          {/*<div className={"analog-clock"}>
-            <div className={"dial seconds"} style={secondsStyle} />
-            <div className={"dial minutes"} style={minutesStyle} />
-            <div className={"dial hours"} style={hoursStyle} />
-          </div> 
-    */}  
-          
-      {
-          <div className={"digital-clock"}>
+          <AnalogClock time={time}></AnalogClock>      
+          <div>
             {time[0]}:{time[1]}:{time[2]}
           </div>
-      }
+      
       </div>
     )
 }
