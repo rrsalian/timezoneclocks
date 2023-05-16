148e61da793337c2e953772316aaea860e041541
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-05-15 21:23:51 -0400

    App.css

--- src/components/Clk/Clk.tsx
+++ src/components/Clk/Clk.tsx
@@ -9,19 +9,15 @@ export function Clk(props: {clock: Clock, onDelete : (clock: Clock) => void}) {
     return (        
         <table className="table">
             <thead></thead>
-            <tbody>
-            <tr className="colHeader">
-                <th className="colHeader"><b>Settings</b></th>
-                <th className="colHeader"><b>Clocks</b></th>
-            </tr>
-            <tr className="row">
-                <td className="settings">
-                    <ClockSettings clock={props.clock} onDelete={props.onDelete}></ClockSettings>
-                </td>
-                <td className="display">
-                    <ClockDisplay clock={props.clock}></ClockDisplay>
-                </td>
-            </tr>
+            <tbody>            
+                <tr className="row">
+                    <td className="settings">
+                        <ClockSettings clock={props.clock} onDelete={props.onDelete}></ClockSettings>
+                    </td>
+                    <td className="display">
+                        <ClockDisplay clock={props.clock}></ClockDisplay>
+                    </td>
+                </tr>
             </tbody>
             <tfoot></tfoot>
         </table>
--- src/components/ClockList/ClockList.tsx
+++ src/components/ClockList/ClockList.tsx
@@ -5,8 +5,19 @@ export function ClockList(props: { clocks: Clock[] , onDelete : (clock: Clock) =
     let title;
   
     if (props.clocks.length > 0)
-        {title = <h3>Clocks</h3>;}
-
+        {title = <h3>   
+                    <table className="table">
+                        <thead></thead>
+                        <tbody>
+                        <tr className="colHeader">
+                            <th className="colHeader">Settings</th>
+                            <th className="colHeader">Clocks</th>
+                        </tr>
+                        </tbody>
+                        <tfoot></tfoot>
+                    </table>
+                </h3>
+        };
     return (
         <div>            
             {title}            
