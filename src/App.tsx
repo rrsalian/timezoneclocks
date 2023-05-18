eaaa1117b272a3ee43026756b63ee008d878d0ad
Omar Mahmoud <omar.mahmoud32@gmail.com>
2023-05-17 21:35:42 -0400

    Fixed Error Timezones

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,48 @@
 //import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Modal from 'react-modal';
-import { Clock } from './models/Clock';
-import { ClockForm  } from './components/ClockForm/ClockForm';
-import { ClockList } from './components/ClockList/ClockList';
-import { TimeZone } from './models/TimeZone';
+import "./App.css";
+import { useState } from "react";
+import Modal from "react-modal";
+import { Clock } from "./models/Clock";
+import { ClockForm } from "./components/ClockForm/ClockForm";
+import { ClockList } from "./components/ClockList/ClockList";
+import { TimeZone } from "./models/TimeZone";
 
 const customStyles = {
-  content: {    
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: '#F9F9A1',
-    border: '3px solid grey', 
-    borderRadius: '5px',
-    width: '300px',
-    height: '250px',
-    fontSize: '18px',
-    fontFamily: 'Arial, sans-serif',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center'
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#F9F9A1",
+    border: "3px solid grey",
+    borderRadius: "5px",
+    width: "300px",
+    height: "250px",
+    fontSize: "18px",
+    fontFamily: "Arial, sans-serif",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 function App() {
-  
   const [modalIsOpen, setIsOpen] = useState(false);
-  const [clocks, setClocks ] = useState<Clock[]>([]);
-  
-  const timeZones:TimeZone[] = [
-    { label: 'New York (EST)', value: "America/New_York" },
-    { label: 'Chicago (CST)', value: 'America/Chicago' },
-    { label: 'Denver (MST)', value: 'America/Denver' },
-    { label: 'Los Angeles (PST)', value: "America/Los_Angeles" },
-    { label: 'London (GMT)', value: 'Europe/London' },
-    { label: 'Paris (CET)', value: 'Europe/Paris' },
-    { label: 'Tokyo (JST)', value: 'Asia/Tokyo' }
+  const [clocks, setClocks] = useState<Clock[]>([]);
+
+  const timeZones: TimeZone[] = [
+    { label: "New York (EST)", value: "America/New_York" },
+    { label: "Chicago (CST)", value: "America/Chicago" },
+    { label: "Denver (MST)", value: "America/Denver" },
+    { label: "Los Angeles (PST)", value: "America/Los_Angeles" },
+    { label: "London (GMT)", value: "Europe/London" },
+    { label: "Paris (CET)", value: "Europe/Paris" },
+    { label: "Tokyo (JST)", value: "Asia/Tokyo" },
   ];
 
   const [tZS, setTZS] = useState(timeZones);
@@ -52,51 +51,55 @@ function App() {
     setIsOpen(false);
   }
 
- function addClock(newClock: Clock):void {
+  function addClock(newClock: Clock): void {
     const i = clocks.length;
     let lastId = 0;
-    
-    if (i > 0)
-      lastId = clocks[i - 1].id;
 
-    
+    if (i > 0) lastId = clocks[i - 1].id;
+
     if (lastId !== undefined) {
-      newClock = { ...newClock, id: lastId + 1};
+      newClock = { ...newClock, id: lastId + 1 };
       setClocks([...clocks, newClock]);
-      setTZS(tZS.filter(t => t.value !== newClock.timeZone));
-    }      
+      setTZS(tZS.filter((t) => t.value !== newClock.timeZone));
+    }
   }
 
-  function deleteClock(clock:Clock): void {
-    
-    const newValue = timeZones.find(c => c.value === clock.timeZone);
-    if (newValue !== undefined) 
-      setTZS([...tZS, newValue]);
-    setClocks(clocks.filter(c => c.id !== clock.id));
+  function deleteClock(clock: Clock): void {
+    const newValue = timeZones.find((c) => c.value === clock.timeZone);
+    if (newValue !== undefined) setTZS([...tZS, newValue]);
+    setClocks(clocks.filter((c) => c.id !== clock.id));
   }
-  
+
   return (
     <div className="App">
       <h1>Clock App</h1>
-      <button onClick={() => setIsOpen(true)}>Add Clock</button>      
-      
+      <button onClick={() => setIsOpen(true)}>Add Clock</button>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
-        contentLabel="Example Modal">  
-
-        <ClockForm onClose={() => setIsOpen(false)} timeZones={tZS} onSubmitForm={(newClock: Clock) =>{addClock(newClock)}}></ClockForm>
+        contentLabel="Example Modal"
+      >
+        <ClockForm
+          onClose={() => setIsOpen(false)}
+          timeZones={tZS}
+          onSubmitForm={(newClock: Clock) => {
+            addClock(newClock);
+          }}
+        ></ClockForm>
       </Modal>
       <div>
         {
-         <ClockList clocks={clocks} onDelete={deleteClock} timeZones={tZS}></ClockList>
+          <ClockList
+            clocks={clocks}
+            onDelete={deleteClock}
+            timeZones={tZS}
+          ></ClockList>
         }
       </div>
-
     </div>
   );
 }
 
 export default App;
-
--- src/components/ClockForm/ClockForm.tsx
+++ src/components/ClockForm/ClockForm.tsx
@@ -3,44 +3,73 @@ import { Clock } from "../../models/Clock";
 import { TimeZone } from "../../models/TimeZone";
 import "./ClockForm.css";
 
-export function ClockForm( props: { onSubmitForm: (clock: Clock) => void , timeZones:TimeZone[], onClose: () => void } ) {
+export function ClockForm(props: {
+  onSubmitForm: (clock: Clock) => void;
+  timeZones: TimeZone[];
+  onClose: () => void;
+}) {
+  const tZS: TimeZone[] = props.timeZones;
+  const [timeZone, setTimeZone] = useState(tZS.length > 0 ? tZS[0].value : "");
+  const [isDigital, setIsDigital] = useState(false);
 
-    const tZS:TimeZone[] = props.timeZones;
-    const [timeZone, setTimeZone] = useState(tZS[0].value);
-    const [isDigital, setIsDigital] = useState(false);
+  function handleFormSubmit(e: any) {
+    e.preventDefault();
 
-    function handleFormSubmit(e: any) {
-        e.preventDefault();
-        
-        const clock = {
-            id : 0,
-            timeZone: timeZone,
-            isDigital: isDigital
-        };
-        props.onSubmitForm(clock);
-        props.onClose();
-    }
+    const clock = {
+      id: 0,
+      timeZone: timeZone,
+      isDigital: isDigital,
+    };
+    props.onSubmitForm(clock);
+    props.onClose();
+  }
 
+  if (tZS.length === 0) {
     return (
-        <div className="modal">
-            <div className="button-wrap">
-            <button className="x-button" onClick={() => props.onClose()}>X</button>
-            </div>
-            <form onSubmit={handleFormSubmit}>        
-                <div>
-                    <label>TimeZone</label>
-                </div>
-                <select id="timezone-select" value={timeZone} onChange={e => setTimeZone(e.target.value)}>
-                    {tZS.map((timezone) => (
-                        <option key={timezone.value} value={timezone.value}>{timezone.label}</option>
-                        ))}
-                </select>
-                <div className="digital-button">
-                    <input type='checkbox' checked={isDigital} onChange={e => setIsDigital(e.target.checked)}></input>
-                    <label>Digital?</label>
-                </div>
-                <button>Add</button>
-            </form>        
+      <div className="modal">
+        <div className="button-wrap">
+          <button className="x-button" onClick={() => props.onClose()}>
+            X
+          </button>
         </div>
-    )
+        <p><b>No More Timezones Available</b></p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="modal">
+      <div className="button-wrap">
+        <button className="x-button" onClick={() => props.onClose()}>
+          X
+        </button>
+      </div>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>TimeZone</label>
+        </div>
+        <select
+          id="timezone-select"
+          value={timeZone}
+          onChange={(e) => setTimeZone(e.target.value)}
+        >
+          {tZS.map((timezone) => (
+            <option key={timezone.value} value={timezone.value}>
+              {timezone.label}
+            </option>
+          ))}
+        </select>
+        <div className="digital-button">
+          <input
+            className="digital-check"
+            type="checkbox"
+            checked={isDigital}
+            onChange={(e) => setIsDigital(e.target.checked)}
+          />
+          <label>Digital?</label>
+        </div>
+        <button className="add-button">Add</button>
+      </form>
+    </div>
+  );
 }
